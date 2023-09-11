0ac89c27d77c0aad322ed5f234f2c69678a4c76d
LK777-D <luka.dolbaia17@gmail.com>
2023-09-11 13:55:56 +0400

    slider

--- src/components/Bestseller.jsx
+++ src/components/Bestseller.jsx
@@ -1,22 +1,73 @@
 import "./Bestsellers.css";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import ProductCard from "./ProductCard";
+/* eslint-disable react/prop-types */
+
+import Slider from "react-slick";
 
 const Bestseller = (props) => {
-  const bestsellerItems = props.prods.slice(8, 12);
-
+  const DiscountedProds = props.prods.filter(
+    (product) => product.discountPercentage > 10
+  );
+  const settings = {
+    dots: true,
+    infinite: false,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 4,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          initialSlide: 2,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
   return (
-    <div className="bestsellerwrapper">
+    <>
       <h2>Top Bestsellers Of The 2023</h2>
-      <div className="bestseller">
-        {bestsellerItems.map((item) => (
-          <div key={item.id} className="bestselleritem">
-            <div>
-              <img src={item.images[1]} alt="product" />
-              <span className="hoverprice">${item.price}</span>
-            </div>
-          </div>
-        ))}
+      <div className="bestsellerwrapper">
+        <Slider {...settings}>
+          {DiscountedProds.map((item) => (
+            <ProductCard
+              key={item.id}
+              to={`/productdetail/${item.id}`}
+              product={item}
+              onAdd={props.onAdd}
+              image={item.images[0]}
+              title={item.title}
+              description={item.description}
+              price={item.price}
+              // rating={product.rating}
+              category={item.category}
+              discountPercentage={item.discountPercentage}
+              rating={item.rating}
+            />
+          ))}
+        </Slider>
       </div>
-    </div>
+    </>
   );
 };
 
--- src/components/OrderHelper.jsx
+++ src/components/OrderHelper.jsx
@@ -3,32 +3,35 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faCartShopping,
   faCreditCard,
-  faDollar,
   faHome,
   faLaptop,
   //   faShoppingCart,
 } from "@fortawesome/free-solid-svg-icons";
+
 const OrderHelper = () => {
   return (
-    <section className="orderhelper">
-      <h2>How To Shop Online ?</h2>
-      <div className="step">
-        <FontAwesomeIcon icon={faLaptop} className="stepicon" />
-        <span>Find Product You Want</span>
-      </div>
-      <div className="step">
-        <FontAwesomeIcon icon={faCartShopping} className="stepicon" />
-        <span>Click Add To Cart</span>
-      </div>
-      <div className="step">
-        <FontAwesomeIcon icon={faCreditCard} className="stepicon" />
-        <span>Open Cart And Proccess To Payment</span>
-      </div>
-      <div className="step">
-        <FontAwesomeIcon icon={faHome} className="stepicon" />
-        <span>Write Adress You Want The Product To be Delivered</span>
-      </div>
-    </section>
+    <>
+      <h2 className="helperheading font4">How To Shop Online ?</h2>
+      <section className="orderhelper font4">
+        <div className="step">
+          <FontAwesomeIcon icon={faLaptop} className="stepicon" />
+          <span>Find Product You Want</span>
+        </div>
+
+        <div className="step">
+          <FontAwesomeIcon icon={faCartShopping} className="stepicon" />
+          <span>Click Add To Cart</span>
+        </div>
+        <div className="step">
+          <FontAwesomeIcon icon={faCreditCard} className="stepicon" />
+          <span>Open Cart And Proceed To Payment</span>
+        </div>
+        <div className="step">
+          <FontAwesomeIcon icon={faHome} className="stepicon" />
+          <span>Write Adress You Want The Product To be Delivered</span>
+        </div>
+      </section>
+    </>
   );
 };
 
